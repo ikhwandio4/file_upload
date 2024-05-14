6d54c9e74f3834f7ea531d83e3b4a83062f93249
ikhwandio4 <basroni12062018@gmail.com>
2024-05-14 10:25:43 +0700

    jobsheet 12 C. VALIDASI FILE UPLOAD

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -21,28 +21,35 @@ class FileUploadController extends Controller
         // dump($request->berkas);
         // dump($request->file('file'));
         // return "Pemrosesan file upload di sini";
-        if ($request->hasFile('berkas'))
-        {
-            echo "path(): ".$request->berkas->path();
-            echo "<br>";
-            echo "extension(): ".$request->berkas->extension();
-            echo "<br>";
-            echo "getClientOriginalExtension(): ".
-                    $request->berkas->getClientOriginalExtension();
-            echo "<br>";
-            echo "getMimeType(): ".
-            $request->berkas->getMimeType();
-            echo "<br>";
-            echo "getClientOriginalName(): ".
-            $request->berkas->getClientOriginalName();
-            echo "<br>";
-            echo "getSize(): ".$request->berkas->getSize();
+
+        $request->validate([
+            'berkas' => 'required|file|image|max:5000',
+        ]);
+
+        echo $request->berkas->getClientOriginalName(). "lolos validasi";
+
+        // if ($request->hasFile('berkas'))
+        // {
+        //     echo "path(): ".$request->berkas->path();
+        //     echo "<br>";
+        //     echo "extension(): ".$request->berkas->extension();
+        //     echo "<br>";
+        //     echo "getClientOriginalExtension(): ".
+        //             $request->berkas->getClientOriginalExtension();
+        //     echo "<br>";
+        //     echo "getMimeType(): ".
+        //     $request->berkas->getMimeType();
+        //     echo "<br>";
+        //     echo "getClientOriginalName(): ".
+        //     $request->berkas->getClientOriginalName();
+        //     echo "<br>";
+        //     echo "getSize(): ".$request->berkas->getSize();
             
 
-        }
-        else {
-            echo "Tidak ada berkas yang diupload";
-        }
+        // }
+        // else {
+        //     echo "Tidak ada berkas yang diupload";
+        // }
 
     }
 }
