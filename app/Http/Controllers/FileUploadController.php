48ebfd5f1f2ab8ba9b801bcbe10506f8ac5f687a
ikhwandio4 <basroni12062018@gmail.com>
2024-05-14 09:53:00 +0700

    jobsheet 12 persiapan awal

---
+++ app/Http/Controllers/FileUploadController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FileUploadController extends Controller
+{
+    //
+    public function fileupload()
+    {
+        return view('file-upload');
+    }
+    public function prosesFileUpload(Request $request)
+    {
+        return "Pemrosesan file upload di sini";
+    }
+}
